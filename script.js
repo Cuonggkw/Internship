56d30baf43c452bd1e48b30d2f4811fb532a0461
Cuonggkw <cp9188950@gmail.com>
2024-06-02 22:18:06 +0700

    project

---
+++ script.js
@@ -0,0 +1,313 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelector('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const button = document.querySelectorAll('.btn');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+///////////////////////////////////////
+// Modal window
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+///////////////////////////////////////
+// Button scrolling
+btnScrollTo.addEventListener('click', function (e) {
+  // về kích thước của một phần tử và vị trí của nó so với khung nhìn.
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+///////////////////////////////////////
+// Page navigation
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   // neo tới giá trị id or class có cùng trên vs href của đường link
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// 1. Thêm trình xử lý sự kiện vào phần tử cha.
+// 2. Xác định yếu tố nào bắt nguồn sự kiện.
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  // preventDefault(): phương thức cho người dùng biết rằng nếu event ko được xử lý thì mặc định sẽ không thực hiện được.
+  e.preventDefault();
+
+  // Matching strategy
+  // Kiểm tra phần tử cha có chứa nav__link ko
+  if (e.target.classList.contains('nav__link')) {
+    // Phương thức trả về giá trị của thuộc tính được chỉ định trên phần tử.
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+///////////////////////////////////////
+// Tabbed component
+tabsContainer.addEventListener('click', function (e) {
+  // closest(duyệt qua phần tử và phần tử gốc của nó).
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach((t) => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach((c) => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Kích hoạt khu vực nội dung.
+  // cung cấp quyền truy cập đọc/ghi vào các thuộc tính dữ liệu tùy chỉnh trên các phần tử.
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`) // tab: chuyển hướng tới trình duyệt.
+    .classList.add('operations__content--active');
+});
+
+///////////////////////////////////////
+// Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    // Duyệt từng phần tử (nav__link) trong (nav)
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    // Duyệt qua phần từ (img) trong (nav).
+    const logo = link.closest('.nav').querySelector('img');
+    // console.log(logo);
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+// bind: cung cấp giá trị.
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+///////////////////////////////////////
+// Sticky navigation: Intersection Observer API
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  // Xác định sự chuyển đổi từ không giao nhau sang giao nhau.
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  // Phần tử được sử dụng làm khung nhìn để kiểm tra mức độ hiển thị của mục tiêu.
+  root: null,
+  // tỷ lệ phần trăm khả năng hiển thị của mục tiêu.
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+  // rootMargin:"-90px"
+});
+// thêm một phần tử vào tập hợp các phần tử đích đang được theo dõi bởi IntersectionObserver
+headerObserver.observe(header);
+
+///////////////////////////////////////
+// Reveal sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  // Phương thức kết thúc việc quan sát một Element.
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  // Thay thế src bằng data-src.
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
+
+///////////////////////////////////////
+// Slider
+const slider = function () {
+  const slides = document.querySelectorAll('.slide');
+  const btnLeft = document.querySelector('.slider__btn--left');
+  const btnRight = document.querySelector('.slider__btn--right');
+  const dotContainer = document.querySelector('.dots');
+
+  // biến của trang hiện tại bằng 0.
+  let curSlide = 0;
+  const maxSlide = slides.length;
+  // console.log(maxSlide);
+
+  // Functions
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        'beforeend',
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach((dot) => dot.classList.remove('dots__dot--active'));
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add('dots__dot--active');
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      // slide=0
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+      // 0%, 100%, 200%
+    );
+  };
+
+  // Next slide.
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const prevSlide = function () {
+    if ((curSlide = 0)) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+
+    activateDot(0);
+  };
+  init();
+
+  // Event handlers
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', nextSlide);
+
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'AltLeft') prevSlide();
+    e.key === 'AltRight' && nextSlide();
+  });
+
+  dotContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots__dot')) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
+};
+slider();
+// console.log(maxSlide);
+///////////////////////////////////////
+// Types of Events and Event Handlers
+const h1 = document.querySelector('h1');
+
+// const alertH1 = function (e) {
+//   alert('addEventListener: Great! You are reading the heading :D');
+
+//   setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+// };
+// Sự kiện được kích hoạt khi đưa con trỏ chuột tới điểm được chỉ định
+// h1.addEventListener('mouseenter', alertH1);
+
+h1.firstElementChild.style.color = 'white';
+// setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// let btn = new MouseEvent('click', {
+//   bubbles: true,
+//   cancelable: true,
+//   view: window,
+// });
+
+// window.addEventListener('beforeunload', function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// });
